14c1759af99099ae7a9303f63aa7248be0c4d85e
Jonas Svarvaa <jonassvarvaa@gmail.com>
2015-06-11 02:25:15 +0200

    Basic reads from ~/.pin complete

--- src/github.com/xolan/pin/list/list.go
+++ src/github.com/xolan/pin/list/list.go
@@ -1,7 +1,35 @@
 package list
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+
+	log "github.com/Sirupsen/logrus"
+	"github.com/mitchellh/go-homedir"
+)
+
+type commands struct {
+	Commands []struct {
+		Command string `json:"command`
+	}
+}
+
+var c = new(commands)
 
 func List() {
-	fmt.Printf("listing...\n")
+	log.Debugln("listing...")
+	var path, _ = homedir.Expand("~/.pin")
+	if jsonFile, err := ioutil.ReadFile(path); err == nil {
+		json.Unmarshal(jsonFile, &c)
+	} else {
+		log.Errorln(err.Error())
+	}
+	printPinned()
+}
+
+func printPinned() {
+	for _, command := range c.Commands {
+		fmt.Println(command.Command)
+	}
 }
--- src/github.com/xolan/pin/pin.go
+++ src/github.com/xolan/pin/pin.go
@@ -1,21 +1,31 @@
 package main
 
 import (
-	"fmt"
+	"os"
+
+	log "github.com/Sirupsen/logrus"
+	"github.com/mitchellh/go-homedir"
 	"github.com/spf13/cobra"
 	"github.com/xolan/pin/list"
-	"os"
 )
 
-func sanity_check(v bool) {
-	if v {
-		fmt.Printf("Checking sanity...\n")
-	}
-	var path = os.ExpandEnv("$HOME/.pin")
+func sanity_check() {
+	log.Debugln("Checking sanity...")
+	var path, _ = homedir.Expand("~/.pin")
 	if _, err := os.OpenFile(path, os.O_RDWR|os.O_CREATE|os.O_EXCL, 0666); !os.IsNotExist(err) {
-		fmt.Printf("Found ~/.pin\n")
-		fmt.Errorf(err.Error())
+		log.Debugf("Found %s", path)
+	} else {
+		log.Errorln(err.Error())
 	}
+}
+
+func config(v bool) {
+	if v {
+		log.SetLevel(log.DebugLevel)
+	} else {
+		log.SetLevel(log.ErrorLevel)
+	}
+	sanity_check()
 }
 
 func main() {
@@ -26,20 +36,21 @@ func main() {
 		Short: "Pin is a command pinner, similar to aliasing",
 		Long:  `Pin is a command pinner, similar to aliasing`,
 		Run: func(cmd *cobra.Command, args []string) {
-			sanity_check(Verbose)
+			config(Verbose)
 		},
 	}
-
-	PinCmd.PersistentFlags().BoolVarP(&Verbose, "verbose", "v", false, "verbose output")
 
 	var ListCmd = &cobra.Command{
 		Use:   "list",
 		Short: "Display a list of pinned commands",
 		Long:  "Display a list of pinned commands",
 		Run: func(cmd *cobra.Command, args []string) {
+			config(Verbose)
 			list.List()
 		},
 	}
+
+	PinCmd.PersistentFlags().BoolVarP(&Verbose, "verbose", "v", false, "verbose output")
 	PinCmd.AddCommand(ListCmd)
 	PinCmd.Execute()
 }
