394ffd9cd61774eb53f98423b22cafc07ffa8f2c
Jonas Svarvaa <jonassvarvaa@gmail.com>
2015-06-14 22:36:10 +0200

    tmp
    tmp

--- src/github.com/xolan/pin/list/list.go
+++ src/github.com/xolan/pin/list/list.go
@@ -1,9 +1,11 @@
 package list
 
 import (
+	"crypto/sha1"
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
+	"os"
 
 	log "github.com/Sirupsen/logrus"
 	"github.com/mitchellh/go-homedir"
@@ -12,25 +14,54 @@ import (
 type commands struct {
 	Commands []struct {
 		Command string `json:"command"`
+		Hash    string `json:"hash"`
 	}
 }
 
 var c = new(commands)
 
+func localFileExists() bool {
+	var retval = false
+	finfo, err := os.Stat("./.pin")
+	if err != nil {
+		log.Debugln("no local .pin")
+		retval = false
+		return retval
+	}
+	if finfo.IsDir() {
+		log.Debugln("no local .pin")
+		retval = false
+	} else {
+		log.Debugln("local .pin")
+		retval = true
+	}
+	return retval
+}
+
 // List pinned commands defined in ./.pin in found, else ~/.pin
 func List() {
-	log.Debugln("listing...")
+	log.Debugln("listing from home...")
 	var path, _ = homedir.Expand("~/.pin")
 	if jsonFile, err := ioutil.ReadFile(path); err == nil {
 		json.Unmarshal(jsonFile, &c)
 	} else {
 		log.Errorln(err.Error())
 	}
+	log.Debugln("listing local...")
+	if localFileExists() {
+		if jsonFile, err := ioutil.ReadFile("./.pin"); err == nil {
+			json.Unmarshal(jsonFile, &c)
+		} else {
+			log.Errorln(err.Error())
+		}
+	}
+
 	printPinned()
 }
 
 func printPinned() {
-	for _, command := range c.Commands {
-		fmt.Println(command.Command)
+	for n, command := range c.Commands {
+		var hash = sha1.Sum([]byte(command.Command))
+		fmt.Printf("[%x] %d: %s\n", hash, n, command.Command)
 	}
 }
